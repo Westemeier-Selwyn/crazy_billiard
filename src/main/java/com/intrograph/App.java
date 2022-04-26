9abede86cae5e15b086f4ca0246ac4999d960a30
Selwyn Westemeier <selwyn.westemeier.001@student.uni.lu>
2022-04-26 16:53:13 +0200

    Point left

--- src/main/java/com/intrograph/App.java
+++ src/main/java/com/intrograph/App.java
@@ -2,24 +2,15 @@ package com.intrograph;
 
 import java.util.Random;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
 import javafx.animation.PathTransition;
-import javafx.animation.Timeline;
+import javafx.animation.SequentialTransition;
 import javafx.application.Application;
-import javafx.geometry.Bounds;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.CubicCurve;
-import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -30,11 +21,12 @@ public class App extends Application {
     Scene scene = new Scene(canvas, 800, 800, Color.BEIGE);
     double xHalf = scene.getWidth()/2;
     double yHalf = scene.getHeight()/2;
+    int po = 0;
+
 
     @Override
     public void start(Stage stage) {
     	    	
-        rectangle.relocate(xHalf, yHalf*2);
         Line xLine = new Line(0, yHalf, xHalf*2, yHalf);
         Line yLine = new Line(xHalf, 0, xHalf, yHalf*2);
         
@@ -44,50 +36,62 @@ public class App extends Application {
 
         stage.setTitle("Crazy Billiard");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
-
-
-        PathTransition path = new PathTransition();
-        int positio = 2;
+            
+        SequentialTransition sq = new SequentialTransition(p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po),p(po));
+        sq.play();
         
-            path.setNode(rectangle);
-            path.setDuration(Duration.seconds(3));
-            path.setPath(setRndCurve(positio));
-            path.setCycleCount(1);
-            path.play();
-
     }
+
+    public PathTransition p(int i)
+    {
+        PathTransition path = new PathTransition();
+            path.setNode(rectangle);
+            path.setDuration(Duration.millis(500));
+            path.setPath(setRndCurve(i));
+            path.setCycleCount(1);
+            po++;
+            if(po == 4)
+            {
+                po = 0;
+            }
+        return path;
+    }
+
+
+
     public CubicCurve setRndCurve(int posi)
     {
         Random random = new Random();
         CubicCurve curve = new CubicCurve();
-        curve.setStartX(rectangle.getX());
-        curve.setStartY(rectangle.getY());
+        curve.setControlX1(random.nextInt((int) xHalf) + xHalf/2);
+        curve.setControlY1(random.nextInt((int) yHalf) + yHalf/2);
         switch(posi)
         {
             case 0:
-            curve.setControlX1(random.nextInt((int) xHalf) - xHalf/2);
-            curve.setControlY1(random.nextInt((int) yHalf) + yHalf/2);
-            curve.setEndX(xHalf);
+            curve.setStartX(xHalf);
+            curve.setStartY(0);
+            curve.setEndX(xHalf*2);
             curve.setEndY(yHalf);
             break;
             case 1:
-            curve.setControlX1(-random.nextInt((int) xHalf) - xHalf/2);
-            curve.setControlY1(random.nextInt((int) yHalf) - yHalf/2);
-            curve.setEndX(-xHalf);
-            curve.setEndY(yHalf);
+            curve.setStartX(xHalf*2);
+            curve.setStartY(yHalf);
+            curve.setEndX(xHalf);
+            curve.setEndY(yHalf*2);
             break;
             case 2:
-            curve.setControlX1(random.nextInt((int) xHalf) - xHalf/2);
-            curve.setControlY1(-random.nextInt((int) yHalf) - yHalf/2);
-            curve.setEndX(-xHalf);
-            curve.setEndY(-yHalf);
+            curve.setStartX(xHalf);
+            curve.setStartY(yHalf*2);
+            curve.setEndX(0);
+            curve.setEndY(yHalf);
             break;
             case 3:
-            curve.setControlX1(random.nextInt((int) xHalf) + xHalf/2);
-            curve.setControlY1(random.nextInt((int) yHalf) - yHalf/2);
+            curve.setStartX(0);
+            curve.setStartY(yHalf);
             curve.setEndX(xHalf);
-            curve.setEndY(yHalf);
+            curve.setEndY(0);
             break;
         }
         
